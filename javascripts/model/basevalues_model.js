7b95c7d3e16cda8e4d5bed92dbcab2b13f735eb2
Sven Mkw <sven.mkw@gmail.com>
2015-04-07 19:13:13 +0200

    moved attributes into defaults

--- javascripts/model/basevalues_model.js
+++ javascripts/model/basevalues_model.js
@@ -1,66 +1,66 @@
 define(["backbone", "underscore" ], function(Backbone, _) {
     var BaseValues = Backbone.Model.extend({
       // data attributes
-      carclasses: [
-        { name: "EstateCar", value: "EstateCar", score: 0 },
-        { name: "Cabrio", value: "Cabrio", score: 0 },
-        { name: "OffRoad", value: "OffRoad", score: 0 },
-        { name: "Limousine", value: "Limousine", score: 0 },
-        { name: "SmallCar", value: "SmallCar", score: 0 },
-        { name: "SportsCar", value: "SportsCar", score: 0 },
-        { name: "Van", value: "Van", score: 0 },
-        { name: "OtherCar", value: "OtherCar", score: 0 },
-      ],
-      colors: [
-        { name: "beige", value: "beige", score: 0 },
-        { name: "gold", value: "gold", score: 0 },
-        { name: "violett", value: "violett", score: 0 },
-        { name: "yellow", value: "yellow", score: 0 },
-        { name: "black", value: "black", score: 0 },
-        { name: "green", value: "green", score: 0 },
-        { name: "red", value: "red", score: 0 },
-        { name: "blue", value: "blue", score: 0 },
-        { name: "gray", value: "gray", score: 0 },
-        { name: "silver", value: "silver", score: 0 }
-      ],
-      seats: [
-        { name: "2", value: 2, score: 0 },
-        { name: "4", value: 4, score: 0 },
-        { name: "5", value: 5, score: 0 },
-        { name: "7", value: 7, score: 0 },
-        { name: "8", value: 8, score: 0 },
-        { name: "9", value: 9, score: 0 }
-      ],
-      priceclasses: [
-        { name: "0", value: { from: 0, to: 2000 }, score: 0 },
-        { name: "2001", value: { from: 2001, to: 4000 }, score: 0 },
-        { name: "4001", value: { from: 4001, to: 8000 }, score: 0 },
-        { name: "8001", value: { from: 8001, to: 12000 }, score: 0 },
-        { name: "12001", value: { from: 12001, to: 20000 }, score: 0 },
-        { name: "20001", value: { from: 20001, to: 30000 }, score: 0 },
-        { name: "30001", value: { from: 30001, to: 50000 }, score: 0 },
-        { name: "50001", value: { from: 50001, to: 90000 }, score: 0 }
-      ],
+      defaults: {
+        carclasses: [
+          { name: "EstateCar", value: "EstateCar", score: 0 },
+          { name: "Cabrio", value: "Cabrio", score: 0 },
+          { name: "OffRoad", value: "OffRoad", score: 0 },
+          { name: "Limousine", value: "Limousine", score: 0 },
+          { name: "SmallCar", value: "SmallCar", score: 0 },
+          { name: "SportsCar", value: "SportsCar", score: 0 },
+          { name: "Van", value: "Van", score: 0 },
+          { name: "OtherCar", value: "OtherCar", score: 0 },
+        ],
+        colors: [
+          { name: "beige", value: "beige", score: 0 },
+          { name: "gold", value: "gold", score: 0 },
+          { name: "violett", value: "violett", score: 0 },
+          { name: "yellow", value: "yellow", score: 0 },
+          { name: "black", value: "black", score: 0 },
+          { name: "green", value: "green", score: 0 },
+          { name: "red", value: "red", score: 0 },
+          { name: "blue", value: "blue", score: 0 },
+          { name: "gray", value: "gray", score: 0 },
+          { name: "silver", value: "silver", score: 0 }
+        ],
+        seats: [
+          { name: "2", value: 2, score: 0 },
+          { name: "4", value: 4, score: 0 },
+          { name: "5", value: 5, score: 0 },
+          { name: "7", value: 7, score: 0 },
+          { name: "8", value: 8, score: 0 },
+          { name: "9", value: 9, score: 0 }
+        ],
+        priceclasses: [
+          { name: "0", value: { from: 0, to: 2000 }, score: 0 },
+          { name: "2001", value: { from: 2001, to: 4000 }, score: 0 },
+          { name: "4001", value: { from: 4001, to: 8000 }, score: 0 },
+          { name: "8001", value: { from: 8001, to: 12000 }, score: 0 },
+          { name: "12001", value: { from: 12001, to: 20000 }, score: 0 },
+          { name: "20001", value: { from: 20001, to: 30000 }, score: 0 },
+          { name: "30001", value: { from: 30001, to: 50000 }, score: 0 },
+          { name: "50001", value: { from: 50001, to: 90000 }, score: 0 }
+        ]
+      },
 
       // converters
       addBaseValue: function(category, name) {
-        _.find(this[category], name).score += 1;
+        _.find(this.attributes[category], name).score += 1;
       },
 
       resetBaseValue: function(category, name) {
-        _.find(this[category], name).score = 0;
+        _.find(this.attributes[category], name).score = 0;
       },
 
       getOverallScore: function() {
         return _(this.attributes).pluck("score")
-          .reduce(values, function(memo, num){return memo + num;}, 0)
-          .value();
+          .reduce(values, function(memo, num){return memo + num;}, 0);
       },
 
       getFullScore: function(category) {
-        return _(this[category]).pluck("score")
-          .reduce(function(memo, num) { return memo + num; }, 0)
-          .value();
+        return _(this.attributes[category]).pluck("score")
+          .reduce(function(memo, num) { return memo + num; }, 0);
       },
 
       getAverageOf: function(category) {
@@ -69,7 +69,7 @@ define(["backbone", "underscore" ], function(Backbone, _) {
         }
 
         var average = [];
-        var sorted = _.sort(this[category], "score");
+        var sorted = _.sortBy(this.attributes[category], "score");
         var fullScore = this.getFullScore(category);
         var score = 0;
 
@@ -88,7 +88,7 @@ define(["backbone", "underscore" ], function(Backbone, _) {
         }
 
         var min = [];
-        var sorted = _.sort(this[category], "score").reverse();
+        var sorted = _.sortBy(this.attributes[category], "score").reverse();
         var fullScore = this.getFullScore(category);
         var score = 0;
 
@@ -107,7 +107,7 @@ define(["backbone", "underscore" ], function(Backbone, _) {
         }
 
         var max = [];
-        var sorted = _.sort(this[category], "score");
+        var sorted = _.sortBy(this.attributes[category], "score");
         var fullScore = this.getFullScore(category);
         var score = 0;
 
