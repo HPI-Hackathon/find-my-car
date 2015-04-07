26799ae7ae8d76f39bedc676519eab17b419dbf6
Sven Mkw <sven.mkw@gmail.com>
2015-04-07 17:58:50 +0200

    averageCartype generator in collection

--- javascripts/collection/cartype_collection.js
+++ javascripts/collection/cartype_collection.js
@@ -1,17 +1,26 @@
-define(["backbone", "model/cartype"], function(Backbone, Cartype) {
-    var CartypeCollection = Backbone.Collection.extend({
-        model: Cartype,
+define(["backbone", "model/cartype_model", "model/basevalues_model"], function(Backbone, CartypeModel, BaseValues) {
+    return Backbone.Collection.extend({
+        // only contains only models computed from routes
+        // so only carclasses and seats can be answered
+        model: CartypeModel,
 
         generateAverageCartype: function() {
-            var averageCartype = new Cartype();
-            var properties = {
-                carclasses: [],
-                seats: []
-            };
+          // resulting cartype
+          var averageCartype = new CartypeModel();
+          var baseValues = new BaseValues();
 
-            _(this.models).each(function(model) {
+          _(this.models).each(function(cartype) {
+            baseValues.addBaseValue("carclasses", cartype.carclass);
+            baseValues.addBaseValue("seats", cartype.seats);
+          });
 
-            });
+          // evaluate the most fitting cartypes and seats
+          averageCartype.carclasses = baseValues.getAverageOf("carclasses");
+          averageCartype.min_seats = baseValues.getMinOf("seats");
+          averageCartype.max_seats = baseValues.getMaxOf("seats");
+
+          // set count of tracked cartypes
+          averageCartype.score = this.models.length;
         }
     });
 });
