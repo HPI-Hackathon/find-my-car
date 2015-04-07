a12dae53f6264498663fc15d44c98690ceed854a
Sven Mkw <sven.mkw@gmail.com>
2015-04-08 01:00:08 +0200

    blacklist fix

--- javascripts/service/blacklist_service.js
+++ javascripts/service/blacklist_service.js
@@ -1,25 +1,28 @@
 /*
-Function which creates a new list 
+Function which creates a new list
 */
-define(["app", "underscore", "collection/blacklist_collection"], function(app, _, Blacklist_collection) {
+define(["app", "underscore", "collection/blacklist_collection"], function(app, _, BlacklistCollection) {
   var BlacklistService = function() {
     this.blacklist = {};
   };
 
   _.extend(BlacklistService.prototype, {
-  
+
     addId: function(id){
       this.blacklist[id] = id;
     },
 
     createAdsList: function(models){
       var adsList = [];
-      _.each(models, function(model){    
-        if(!_.contains(this.blacklist, model.get("id"))){
-         adsList.push(model);
+      var self = this;
+
+      _.each(models, function(model) {
+        if (!_.contains(self.blacklist, model.get("id"))) {
+          adsList.push(model);
         }
       });
-      return new Blacklist_collection(adsList);
+
+      return new BlacklistCollection(adsList);
     }
 
   });
