a8eab4baf6b87a82420dc8e036573bcfce2d81d8
Sven Mkw <sven.mkw@gmail.com>
2015-04-07 15:43:32 +0200

    moved data to js folder

---
+++ javascripts/data/routes.js
@@ -0,0 +1,59 @@
+define("data-routes", function() {
+  return {
+    "routes": [
+      {
+        "surrounding": "city",
+        "distance": 3200,
+        "start": 1427965900,
+        "end":    1427966400,
+        "groupsize": 1
+      },
+      {
+        "surrounding": "city",
+        "distance": 3200,
+        "start": 1427967000,
+        "end":    1427967300,
+        "groupsize": 1
+      },
+
+      {
+      "surrounding": "highway",
+      "distance": 32000,
+      "start": 1427971200,
+      "end":    1427974500,
+      "groupsize": 3
+      },
+
+      {
+      "surrounding": "city",
+      "distance": 2100,
+      "start": 1427986800,
+      "end":    1427987700,
+      "groupsize": 4
+      },
+      {
+      "surrounding": "city",
+      "distance": 2100,
+      "start": 1427993400,
+      "end":    1427994300,
+      "groupsize": 4
+      },
+
+      {
+      "surrounding": "highway",
+      "distance": 32000,
+      "start": 1428001500,
+      "end":    1428005400,
+      "groupsize": 3
+      },
+
+      {
+      "surrounding": "rural",
+      "distance": 4100,
+      "start": 1428005460,
+      "end":    1428006300,
+      "groupsize": null
+      }
+    ]
+  };
+});
