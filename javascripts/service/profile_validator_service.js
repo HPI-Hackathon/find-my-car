55ade0caf2868f777f1c47b920e8a25fbef9b0bf
Sven Mkw <sven.mkw@gmail.com>
2015-04-07 21:45:22 +0200

    no static obj

--- javascripts/service/profile_validator_service.js
+++ javascripts/service/profile_validator_service.js
@@ -1,12 +1,17 @@
-define([ "underscore" ], function() {
+define(function() {
+  var ProfileValidatorService = function() {
 
-	return {
-		validateProfile: function(averageCarType, userProfile){
+  };
+
+	_.extend(ProfileValidatorService, {
+		validateProfile: function(averageCarType, userProfile) {
 			var min_max_prices = userProfile.getMinMaxPrice();
 			averageCarType.min_price = min_max_prices.min;
 			averageCarType.max_price = min_max_prices.max;
 			averageCarType.colors = [userProfile.color];
 			averageCarType.extras = userProfile.extras;
 		}
-	};
+  });
+
+  return ProfileValidatorService;
 });
