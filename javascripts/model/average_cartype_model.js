3d0790230a7431c642abaa16f29e82cce9b1b1cb
masern <martin.fischer@student.hpi.uni-potsdam.de>
2015-04-07 20:16:25 +0200

    merge fixed

--- javascripts/model/average_cartype_model.js
+++ javascripts/model/average_cartype_model.js
@@ -10,14 +10,13 @@ define(["backbone"], function(Backbone) {
         colors: [],
         extras: {},
         score: 0, // if Routes: how many routes where gathered? if Rating: how many iterations?
-      }
+      },
 
       //expectation: ohterCartype is always the rating avgCartype
       merge: function(otherCartype) {
-        var weighting = (1 - Math.min(otherCartype.get("score")/100.0, 1.0));
-        weighting = 0.5;
-        this.set("min_price", this.get("min_price") * weighting + otherCartype.get("min_price") *(1 - weighting));
-        this.set("max_price", this.get("max_price") * weighting + otherCartype.get("max_price") *(1 - weighting));
+        var weighting = (1 - Math.min(otherCartype.get("score")/100, 1)* 0.8);
+        this.set("min_price", parseInt(this.get("min_price") * weighting + otherCartype.get("min_price") *(1 - weighting)));
+        this.set("max_price", parseInt(this.get("max_price") * weighting + otherCartype.get("max_price") *(1 - weighting)));
       
       }
     });
