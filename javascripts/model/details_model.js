37f5101820a9f665e7f00bf0b7d16fb62ed301be
Sven Mkw <sven.mkw@gmail.com>
2015-04-08 09:53:00 +0200

    cleanup

--- javascripts/model/details_model.js
+++ javascripts/model/details_model.js
@@ -1,10 +1,8 @@
 define(["backbone"], function(Backbone) {
-    var DetailsModel = Backbone.Model.extend({
+    return Backbone.Model.extend({
         headers: {
           "Accept-Language": "de"
         },
         url: function() { return "http://m.mobile.de/svc/a/" + this.id; }
     });
-
-    return DetailsModel;
 });
