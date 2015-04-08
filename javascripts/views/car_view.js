79668d3e049ccb85b004cc53578dc7280173b226
Sven Mkw <sven.mkw@gmail.com>
2015-04-08 08:53:20 +0200

    correct view nav

--- javascripts/views/car_view.js
+++ javascripts/views/car_view.js
@@ -1,6 +1,7 @@
 define(["backbone", "app", "text!template/car.html"], function(Backbone, app, template) {
   return Backbone.View.extend({
     events: {
+      "click .close": function() { app.router.navigate("#", { trigger: true}); }
     },
 
     render: function() {
