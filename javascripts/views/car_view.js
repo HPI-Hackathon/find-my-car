dd9b4d6ebfc889fe5d200b3412e454ec786833a5
masern <martin.fischer@student.hpi.uni-potsdam.de>
2015-04-08 08:56:26 +0200

    container for car details

--- javascripts/views/car_view.js
+++ javascripts/views/car_view.js
@@ -3,6 +3,13 @@ define(["backbone", "app", "text!template/car.html"], function(Backbone, app, te
     events: {
     },
 
+    containerCar = {
+    	price: this.detailsModel.get("attr").price.grs.ammount,
+    	seats: this.detailsModel.get("seats"),
+    	color: this.detailsModel.get("attr").ecol,
+    	carclass: this.get("category"),
+    },
+
     render: function() {
       this.$el.html(_.template(template)(this.model.attributes));
       return this;
