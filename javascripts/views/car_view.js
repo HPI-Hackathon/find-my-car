ae80488ec26551776dab042dee3b07df0c8859eb
Sven Mkw <sven.mkw@gmail.com>
2015-04-08 09:48:04 +0200

    next linking

--- javascripts/views/car_view.js
+++ javascripts/views/car_view.js
@@ -1,12 +1,34 @@
 define(["backbone", "app", "text!template/car.html"], function(Backbone, app, template) {
   return Backbone.View.extend({
     events: {
-      "click .close": function() { app.router.navigate("#", { trigger: true}); }
+      "click .close": function() { app.router.navigate("#", { trigger: true}); },
+      "click #next": "next",
+      "change input[type=checkbox]": "checkboxChange"
     },
 
     render: function() {
       this.$el.html(_.template(template)(this.model.attributes));
       return this;
+    },
+
+    next: function(event) {
+      // do not link the link
+      event.preventDefault();
+
+      // validate all spreadsheet props
+      this.model.finish(this.detailsModel);
+
+      // route the app
+      app.router.navigate("#next", { trigger: true });
+    },
+
+    checkboxChange: function(event) {
+      console.log(event);
+      if (event.target.checked) {
+        this.model.setValue(event.target.name);
+      } else {
+        this.model.unsetValue(event.target.name);
+      }
     }
   });
 });
