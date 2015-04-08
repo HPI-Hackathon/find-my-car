b141b96c1cac6a12df1ff22e9f9af7240d0303f1
masern <martin.fischer@student.hpi.uni-potsdam.de>
2015-04-08 02:39:23 +0200

    validate adds_model + add_baseValue

--- javascripts/model/ads_model.js
+++ javascripts/model/ads_model.js
@@ -1,7 +1,36 @@
-define(["backbone"], function(Backbone) {
+define(["backbone", "app", "underscore"], function(Backbone, app, _) {
     return Backbone.Model.extend({
     //data attributes
+    defauls:{
+    	validateHashes: {}
+    },
 
+    //converter 
+    finish: function(){
+    	//match values
+  		if(_.contains(validateHashes, "attr.ecol")){
+  			app.ratings.addBaseValue("colors", this.get("attr").ecol);
+  		};
+  		if(_.contains(validateHashes, "category")){
+  			app.ratings.addBaseValue("carclasses", this.get("category"));
+  		};
+  		//TODO get seats
+  		if(_.contains(validateHashes, "seats")){
+  			app.ratings.addBaseValue("seats", seats);
+  		};
+  		if(_.contains(validateHashes, "attr.price.grs.ammount")){
+  			app.ratings.addBaseValue("priceclasses", app.ratings.getPriceCategory(this.get("attr").price.grs.ammount));
+  		};
+    	app.blacklist.addId(this.id);
+    },
+
+    setValue: function(key){
+    	validateHashes[key] = key;
+    },
+
+    unsetValue: function(key){
+    	delete validateHashes[key];
+    },
 
     });
 });
