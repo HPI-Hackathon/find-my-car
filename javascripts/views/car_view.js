0936f27b779763f660eb54b6d2495eaba99d1790
masern <martin.fischer@student.hpi.uni-potsdam.de>
2015-04-08 09:28:54 +0200

    helperTemplate object

--- javascripts/views/car_view.js
+++ javascripts/views/car_view.js
@@ -4,11 +4,28 @@ define(["backbone", "app", "text!template/car.html"], function(Backbone, app, te
       "click .close": function() { app.router.navigate("#", { trigger: true}); }
     },
 
-    containerCar = {
-    	price: this.detailsModel.get("attr").price.grs.ammount,
-    	seats: this.detailsModel.get("seats"),
-    	color: this.detailsModel.get("attr").ecol,
-    	carclass: this.get("category"),
+
+    initialize: function(options){
+    	this.detailsModel = options.detailsModel;
+    	this.templateHelper = {
+    		attributes: 
+    			[
+    			  {
+    			 	label: "Autotyp",
+    			 	tag: "carclass",
+    			 	value: this.detailsModel.get("category")
+    			  },
+    			 {
+    			 	label: "Kaufpreis",
+    			 	tag: "price",
+    			 	value: this.detailsModel.get("price").grs.amount
+    			 }
+    			].concat(this.detailsModel.attributes),
+    			images: this.model.get("images"),
+    			title: this.model.get("title"),
+    			url: this.model.get("url"),
+    			description: this.detailsModel.get("description")
+    	};
     },
 
     render: function() {
